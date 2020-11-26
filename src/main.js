144f1547329e7f993501b672ebe2c0479281c5d1
wisemk79 <wisemk79@gmail.com>
2020-11-26 18:00:11 +0900

    idea

--- src/main.js
+++ src/main.js
@@ -18,17 +18,20 @@ export default class DataRandomDancer {
      *
      * 기본 반환타입은 Integer이며
      *
-     * 타입을 string으로 지정하면 String 형태의 숫자를 얻을 수 있다
+     * 타입을 string으로 지정하면 String 형태의 숫자를 얻을 수 있다.
+     *
+     * zeroFixBackDigit을 설정하면 뒷자리수를 기준으로 몇자리를 0으로 지정할 것인지 설정할 수 있다.
+     *
+     * zeroFixBackDigit은 digit보다 작아야한다.
      *
      * (digit: 자릿수, type?:"string" or undefined)
      */
-    getRanDigitNum(digit, type) {
+    getRanDigitNum(digit, type, zeroFixBackDigit) {
         let number;
         while (true) {
             number = "";
             for (let i = 0; i < digit; i++) {
                 let num = Math.floor(Math.random() * 10);
-                console.log(num);
                 number += String(num);
             }
             if (type === "string")
--- src/main.ts
+++ src/main.ts
@@ -25,9 +25,13 @@ export default class DataRandomDancer {
      * 
      * 타입을 string으로 지정하면 String 형태의 숫자를 얻을 수 있다.
      * 
+     * zeroFixBackDigit을 설정하면 뒷자리수를 기준으로 몇자리를 0으로 지정할 것인지 설정할 수 있다.
+     * 
+     * zeroFixBackDigit은 digit보다 작아야한다.
+     * 
      * (digit: 자릿수, type?:"string" or undefined)
      */
-    getRanDigitNum(digit:number, type?:string) {
+    getRanDigitNum(digit:number, type?:string, zeroFixBackDigit?:number) {
         let number:any;
         while(true){
             number = "";
